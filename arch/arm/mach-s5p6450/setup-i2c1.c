348dce169f01be3b8c2a565aea6670a950891594
Wonyoung Choi <patrick.choi@samsung.com>
2010-07-14 20:43:55 +0900

    ARM: S5P6450: Update Support for I2C
    This patch replaces use of GPIO-API from  direct access of GPIO-register
    because bug about GPIO-map has been fixed.
    
    Signed-off-by: Wonyoung Choi <patrick.choi@samsung.com>

--- arch/arm/mach-s5p6450/setup-i2c1.c
+++ arch/arm/mach-s5p6450/setup-i2c1.c
@@ -1,11 +1,11 @@
-/* linux/arch/arm/mach-s5p6450/setup-i2c0.c
+/* linux/arch/arm/mach-s5p6450/setup-i2c1.c
  *
  * Copyright (c) 2009 Samsung Electronics Co., Ltd.
  *		http://www.samsung.com/
  *
- * I2C0 GPIO configuration.
+ * I2C1 GPIO configuration.
  *
- * Based on plat-s3c64xx/setup-i2c0.c
+ * Based on mach-s5p6450/setup-i2c0.c
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -14,10 +14,6 @@
 
 #include <linux/kernel.h>
 #include <linux/types.h>
-#if 1
-#include <linux/io.h>
-void __iomem *iobase;
-#endif
 
 struct platform_device; /* don't need the contents */
 
@@ -27,28 +23,9 @@ struct platform_device; /* don't need the contents */
 
 void s3c_i2c1_cfg_gpio(struct platform_device *dev)
 {
-#if 0
 	s3c_gpio_cfgpin(S5P6450_GPR(9), S3C_GPIO_SFN(6));
 	s3c_gpio_cfgpin(S5P6450_GPR(10), S3C_GPIO_SFN(6));
 
 	s3c_gpio_setpull(S5P6450_GPR(9), S3C_GPIO_PULL_UP);
 	s3c_gpio_setpull(S5P6450_GPR(10), S3C_GPIO_PULL_UP);
-#else
-	u32 reg_value;
-
-	/* set iobase address */
-	iobase = ioremap(0xe0308290, 0x10);
-
-	/* set GPRCON1 register */
-	reg_value = readl(iobase + 0x04);
-	/* GPR[9] set to I2C_SCL1 and GPR[10] set to I2C_SDA1 */
-	reg_value = ((reg_value & ~(0xFF<<8)) | (0x66<<8));
-	writel(reg_value, (iobase + 0x04));
-
-	/* set GPRPUD register */
-	reg_value = readl(iobase + 0x0C);
-	/* GPR[9], GPR[10] set to PULLUP_ENABLE */
-	reg_value = ((reg_value & ~(0xF<<18)) | (0xA<<18));
-	writel(reg_value, (iobase + 0x0C));
-#endif
 }
