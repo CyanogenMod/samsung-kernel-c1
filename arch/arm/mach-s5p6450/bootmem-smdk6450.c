4897e75b3fae2f58535cceb8e1f0f36d652c2aeb
Jingoo Han <jg1.han@samsung.com>
2010-07-21 13:50:31 +0900

    ARM: S5P6450: Add the bootmem helper functions
    This patch adds the bootmem helper functions which help
    media device to know the information of memory allocation
    and delineate the description for machine specific part
    as to memory allocation for media device.
    
    The description for S5P6450 machine is empty but bootmem
    scheme will be implemented then this description information
    will be described. The file should be in machine directory
    in order to fix build error problem.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>

---
+++ arch/arm/mach-s5p6450/bootmem-smdk6450.c
@@ -0,0 +1,26 @@
+/* linux/arch/arm/mach-s5p6450/bootmem-smdk6450.c
+ *
+ * Copyright (c) 2009 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com/
+ *
+ * Bootmem helper functions for smdk6450
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
+#include <linux/mm.h>
+#include <linux/bootmem.h>
+#include <linux/swap.h>
+#include <asm/setup.h>
+#include <linux/io.h>
+#include <mach/memory.h>
+#include <plat/media.h>
+#include <mach/media.h>
+
+struct s5p_media_device media_devs[] = {
+	/* Will be populated later */
+};
+
+int nr_media_devs = (sizeof(media_devs) / sizeof(media_devs[0]));
