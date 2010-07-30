cfc6590e31203dbe362659d251780ba4c6f7b3aa
Jingoo Han <jg1.han@samsung.com>
2010-07-30 15:26:05 +0900

    ARM: S5PV210: Change USB clock name
    This patch changes USB clock name from 'usb-host'
    to 'usbhost'.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>

--- arch/arm/mach-s5pv210/clock.c
+++ arch/arm/mach-s5pv210/clock.c
@@ -328,7 +328,7 @@ static struct clk init_clocks_disable[] = {
 		.enable		= s5pv210_clk_ip1_ctrl,
 		.ctrlbit	= (1<<16),
 	}, {
-		.name		= "usb-host",
+		.name		= "usbhost",
 		.id		= -1,
 		.parent		= &clk_hclk_psys.clk,
 		.enable		= s5pv210_clk_ip1_ctrl,
