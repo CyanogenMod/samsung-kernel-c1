75ece8b6f089719bfb8af0ce2e1bb264267e6514
Hyuk Lee <hyuk1.lee@samsung.com>
2010-08-16 20:30:46 +0900

    SDHCI: S5PV310: Block quirks only for V310
    This patch is for blocking BROKEN_CLOCK_DIVIDER quirk for other
    platforms.
    
    Signed-off-by: Hyuk Lee <hyuk1.lee@samsung.com>

--- drivers/mmc/host/sdhci-s3c.c
+++ drivers/mmc/host/sdhci-s3c.c
@@ -484,7 +484,9 @@ static int __devinit sdhci_s3c_probe(struct platform_device *pdev)
 	host->quirks |= SDHCI_QUIRK_NO_HISPD_BIT;
 
 	host->quirks |= SDHCI_QUIRK_BROKEN_CARD_DETECTION;
+#ifdef CONFIG_ARCH_S5PV310
 	host->quirks |= SDHCI_QUIRK_BROKEN_CLOCK_DIVIDER;
+#endif
 	ret = sdhci_add_host(host);
 	if (ret) {
 		dev_err(dev, "sdhci_add_host() failed\n");
