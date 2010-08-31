db64ab46da68c40694f890b7a94305ff52f7d53f
Hyunmin Kwak <hyunmin.kwak@samsung.com>
2010-08-31 15:12:57 +0900

    ARM: SAMSUNG: Add platform dependent codes for JPEG drvier.
    This patch adds the platform dependent codes of the JPEG driver
    for S5PVXX platform.
    
    Signed-off-by: Hyunmin Kwak <hyunmin.kwak@samsung.com>

--- arch/arm/plat-s5p/devs.c
+++ arch/arm/plat-s5p/devs.c
@@ -538,6 +538,30 @@ struct platform_device s3c_device_jpeg = {
 EXPORT_SYMBOL(s3c_device_jpeg);
 #endif /* CONFIG_VIDEO_JPEG_V2 */
 
+#ifdef CONFIG_VIDEO_JPEG
+/* JPEG controller  */
+static struct resource s5p_jpeg_resource[] = {
+	[0] = {
+		.start = S5P_PA_JPEG,
+		.end   = S5P_PA_JPEG + S5P_SZ_JPEG - 1,
+		.flags = IORESOURCE_MEM,
+	},
+	[1] = {
+		.start = IRQ_JPEG,
+		.end   = IRQ_JPEG,
+		.flags = IORESOURCE_IRQ,
+	}
+};
+
+struct platform_device s5p_device_jpeg = {
+	.name             = "s5p-jpeg",
+	.id               = -1,
+	.num_resources    = ARRAY_SIZE(s5p_jpeg_resource),
+	.resource         = s5p_jpeg_resource,
+};
+EXPORT_SYMBOL(s5p_device_jpeg);
+#endif /* CONFIG_VIDEO_JPEG */
+
 #ifdef CONFIG_VIDEO_ROTATOR
 /* rotator interface */
 static struct resource s5p_rotator_resource[] = {
--- arch/arm/plat-samsung/include/plat/devs.h
+++ arch/arm/plat-samsung/include/plat/devs.h
@@ -102,6 +102,7 @@ extern struct platform_device s3c_device_usb_hsudc;
 
 extern struct platform_device s5p_device_mfc;
 extern struct platform_device s3c_device_jpeg;
+extern struct platform_device s5p_device_jpeg;
 extern struct platform_device s5p_device_rotator;
 extern struct platform_device s5p_device_fimg2d;
 
