793edaef42612aa34cb7ca008a416432a09cd3a7
Sangpil Moon <sangpil.moon@samsung.com>
2010-09-17 15:57:37 +0900

    ARM: S5PV210: Modify hdmi cec regsiter header file license
    This commit modify hdmi cec register header file license
    
    Signed-off-by: Sangpil Moon <sangpil.moon@samsung.com>

--- arch/arm/mach-s5pv210/include/mach/regs-cec.h
+++ arch/arm/mach-s5pv210/include/mach/regs-cec.h
@@ -1,9 +1,9 @@
 /* linux/drivers/media/video/samsung/tv20/s5pc100/regs/regs-cec.h
  *
- * CEC register header file for Samsung TVOut driver
- *
  * Copyright (c) 2009 Samsung Electronics
  * 	http://www.samsungsemi.com/
+ *
+ * CEC register header file for Samsung TVOut driver
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
